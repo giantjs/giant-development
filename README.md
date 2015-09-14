cf5c227d20b9bcbbd3279ee2098845735f0c5beb
Dan Stocker <dan.stocker@mangahigh.com>
2015-09-14 17:50:06 +0100

    streamlines build tools

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Installation
 Running the following command from the project root will give you the CLI `giant-dev` through which you can run (batch) commands for one or more modules.
 
     npm install -g
-   
+
 ### Karma
 
 It advised to install Karma if you don't have it already.
@@ -25,28 +25,8 @@ It advised to install Karma if you don't have it already.
 CLI Usage
 ---------
 
-The CLI takes two arguments.
-
-1. Which argument to run the command for.
-2. Command to be run for each affected module.
-
 ### Example
 
 Lists the working directories for all Giant modules.
 
-    giant-dev all pwd
-
-### Deployment
-
-These batch commands help you get through a full deployment. (After having set the new version for all modules.)
-
-    giant-dev all "npm install"
-    giant-dev all "git tag v#.#.#"
-    giant-dev all "grunt build"
-    giant-dev all "npm publish"
-
-### Other useful commands
-
-    giant-dev all "npm prune"
-    giant-dev all "git tag -d v#.#.#"
-    giant-dev all "git reset --hard HEAD~1"
+    giant-dev batch pwd
--- scripts/giant-dev.sh
+++ scripts/giant-dev.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $# -lt 2 ]; then
-    echo "Usage:"
-    echo "  giant-dev (<module-name>|all) <command>"
-else
-    GREEN='\x1b[32m'
-    NC='\x1b[0m' # No Color
+CYAN='\x1b[1;36m'
+GREEN='\x1b[32m'
+NC='\x1b[0m' # No Color
 
-    if [[ $1 == all ]]; then
+if [[ $1 == batch ]]; then
+    if [ $# -lt 2 ]; then
+        echo "Usage:"
+        echo "  giant-dev batch <command>"
+    else
+        echo -e "${CYAN}Running batch command${NC} \"$2\""
         cd modules
         for f in * ; do
             if [[ -d $f ]]; then
@@ -17,7 +19,22 @@ else
             fi
         done
         cd ..
-    else
-        (cd modules/$1 && eval $2)
     fi
+elif [[ $1 == clear ]]; then
+    echo -e "${CYAN}Clearing module packages${NC}"
+    ./scripts/giant-dev.sh batch "rm -f *.tgz"
+elif [[ $1 == build ]]; then
+    echo -e "${CYAN}Building modules${NC}"
+    (cat ./scripts/modules.dat | xargs -n 1 ./scripts/build-distribute.sh)
+elif [[ $1 == run ]]; then
+    if [ $# -lt 3 ]; then
+        echo "Usage:"
+        echo "  giant-dev run <module-name> <command>"
+    else
+        echo -e "${CYAN}Running command${NC} \"$3\" ${CYAN}for module${NC} \"$2\""
+        (cd modules/$2 && eval $3)
+    fi
+else
+    echo "Usage:"
+    echo "  giant-dev (run|batch|clear|build) [module-name] [command]"
 fi
